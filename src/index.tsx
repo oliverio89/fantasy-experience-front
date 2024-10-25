d4d1f48fb3a01700af18a92f649f6433d72eeb8c
Victor <victor.moreno@factoriaf5.org>
2024-10-25 17:27:44 +0200

    console.log

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const container = document.getElementById("root");
+console.log(container + "aqui estoy");
 const root = createRoot(container!);
 
 root.render(
